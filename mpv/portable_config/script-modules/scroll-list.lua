9dbe9f0f960d4c5fe26a8f52ebb97cb55ecfe90a
redomCL <1429498645@qq.com>
2024-02-23 18:26:32 +0800

    Update scroll-list.lua

--- mpv/portable_config/script-modules/scroll-list.lua
+++ mpv/portable_config/script-modules/scroll-list.lua
@@ -120,13 +120,13 @@ function scroll_list:update_ass()
     if not overflow then finish = #self.list end
 
     --adding a header to show there are items above in the list
-    if start > 1 then self:append(self.wrapper_style..(start-1)..' item(s) above\\N\\N') end
+    if start > 1 then self:append(self.wrapper_style..(start-1)..'  个文件⬆\\N\\N\\N\\N') end
 
     for i=start, finish do
         self:format_line(i, self.list[i])
     end
 
-    if overflow then self:append('\\N'..self.wrapper_style..#self.list-finish..' item(s) remaining') end
+    if overflow then self:append('\\N'..self.wrapper_style..#self.list-finish..' 个文件⬇') end
     self.ass:update()
 end
 
