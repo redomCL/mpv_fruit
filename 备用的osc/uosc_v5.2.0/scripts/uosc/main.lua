bc063e6280c1a90e2f3135ac886c20e55aebe69a
redomCL <1429498645@qq.com>
2024-08-15 02:37:02 +0800

    Add files via upload

--- 备用的osc/uosc_v5.2.0/scripts/uosc/main.lua
+++ 备用的osc/uosc_v5.2.0/scripts/uosc/main.lua
@@ -7,7 +7,7 @@
 --		for index, item in ipairs(playlist) do
 --			local is_url = is_protocol(item.filename)
 --			items[index] = {
---				title = is_url and item.filename or serialize_path(item.filename).basename,
+--				title = (is_url and item.filename or serialize_path(item.filename).basename),
 --				hint = tostring(index),
 --				active = item.current,
 --				value = index,
@@ -891,7 +891,7 @@ bind_command('playlist', create_self_updating_menu_opener({
 		for index, item in ipairs(playlist) do
 			local is_url = is_protocol(item.filename)
 			items[index] = {
-				title = is_url and item.filename or serialize_path(item.filename).basename,
+				title = (is_url and item.filename or serialize_path(item.filename).basename),
 				hint = tostring(index),
 				active = item.current,
 				value = index,
